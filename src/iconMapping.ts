e2691a844ba5f5ada58519939cde70511e6d20a1
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-23 01:22:26 +0200

    started on iconMapping, will continue mapping for all weather codes

---
+++ src/iconMapping.ts
@@ -0,0 +1,8 @@
+// iconMapping.ts
+export const iconMapping = {
+  '01d': 'pe-7w-sun', // clear sky (day)
+  '01n': 'pe-7w-moon', // clear sky (night)
+  '02d': 'pe-7w-cloud-sun', // few clouds (day)
+  '02n': 'pe-7w-cloud-moon', // few clouds (night)
+  // ... continue mapping for all weather codes
+};
