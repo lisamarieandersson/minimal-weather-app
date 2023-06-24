61e94243468e4c0cb00e4884bf4c37faba36a46c
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-24 22:23:51 +0200

    continued mapping for font icons

--- src/iconMapping.ts
+++ src/iconMapping.ts
@@ -4,5 +4,13 @@ export const iconMapping = {
   '01n': 'pe-7w-moon', // clear sky (night)
   '02d': 'pe-7w-cloud-sun', // few clouds (day)
   '02n': 'pe-7w-cloud-moon', // few clouds (night)
+  '03d': 'pe-7w-cloud', // scattered clouds (day)
+  '03n': 'pe-7w-cloud-moon', // scattered clouds (night)
+  '04d': 'pe-7w-cloud', // broken clouds (day)
+  '04n': 'pe-7w-cloud-moon', // broken clouds (night)
+  '09d': 'pe-7w-drizzle', // shower rain / drizzle (day)
+  '09n': 'pe-7w-drizzle-moon', // shower rain / drizzle (night)
+  '10d': 'pe-7w-rain-alt', // rain (day)
+  '10n': 'pe-7w-rain-alt-moon', // rain (night)
   // ... continue mapping for all weather codes
 };
