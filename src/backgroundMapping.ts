759611461d187361b0b78dff29e3e1138e5f5ee1
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-26 19:36:34 +0200

    added function for mapping background images according to weather conditions / weather icons

---
+++ src/backgroundMapping.ts
@@ -0,0 +1,21 @@
+export const backgroundMapping = {
+  '01d': 'background-8-blue.jpg', // clear sky (day)
+  '01n': 'background-5-multi.jpg', // clear sky (night)
+  '02d': 'background-5-blush.jpg', // few clouds (day)
+  '02n': 'background-17-lavender.jpg', // few clouds (night)
+  //   '03d': 'pe-7w-cloud', // scattered clouds (day)
+  //   '03n': 'pe-7w-cloud-moon', // scattered clouds (night)
+  //   '04d': 'pe-7w-cloud', // broken clouds (day)
+  //   '04n': 'pe-7w-cloud-moon', // broken clouds (night)
+  //   '09d': 'pe-7w-drizzle', // shower rain / drizzle (day)
+  //   '09n': 'pe-7w-drizzle-moon', // shower rain / drizzle (night)
+  //   '10d': 'pe-7w-rain-alt', // rain (day)
+  //   '10n': 'pe-7w-rain-alt-moon', // rain (night)
+  //   '11d': 'pe-7w-lightning', // thunderstorm (day)
+  //   '11n': 'pe-7w-lightning-moon', // thunderstorm (night)
+  //   '13d': 'pe-7w-snow-alt', // snow (day)
+  //   '13n': 'pe-7w-snow-alt-moon', // snow (night)
+  //   '50d': 'pe-7w-fog', // atmosphere like mist, fog etc. (day)
+  //   '50n': 'ppe-7w-fog-moon', // atmosphere like mist, fog etc. (night)
+  // etc...
+};
