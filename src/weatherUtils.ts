eda3d701905404648dfd568400403b70d7825941
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-27 15:14:18 +0200

    exchanged capital letter to lowercase

--- src/weatherUtils.ts
+++ src/weatherUtils.ts
@@ -5,27 +5,27 @@ export function describeWindSpeed(speed: number): string {
   if (speed < 0.3) {
     return 'Calm';
   } else if (speed < 1.5) {
-    return 'Light Air';
+    return 'Light air';
   } else if (speed < 3.3) {
-    return 'Light Breeze';
+    return 'Light breeze';
   } else if (speed < 5.5) {
-    return 'Gentle Breeze';
+    return 'Gentle breeze';
   } else if (speed < 7.9) {
-    return 'Moderate Breeze';
+    return 'Moderate breeze';
   } else if (speed < 10.7) {
-    return 'Fresh Breeze';
+    return 'Fresh breeze';
   } else if (speed < 13.8) {
-    return 'Strong Breeze';
+    return 'Strong breeze';
   } else if (speed < 17.1) {
-    return 'High Wind';
+    return 'High wind';
   } else if (speed < 20.7) {
     return 'Gale';
   } else if (speed < 24.4) {
-    return 'Severe Gale';
+    return 'Severe gale';
   } else if (speed < 28.4) {
     return 'Storm';
   } else if (speed < 32.6) {
-    return 'Violent Storm';
+    return 'Violent storm';
   } else {
     return 'Hurricane';
   }
