c17ec1084c9feeb63f555559593e0c1f23e4d8b1
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-28 23:17:17 +0200

    moved html weather data display with css to its own file

---
+++ src/WeatherDisplay.svelte
@@ -0,0 +1,121 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition';
+
+  export let location: string;
+  export let weather: string;
+  export let temperature: string;
+  export let feelsLike: string;
+  export let humidity: string;
+  export let windSpeed: string;
+  export let windDescription: string;
+  export let lastUpdated: string;
+  export let weatherIcon: string;
+  export let currentTime: Date;
+</script>
+
+<!-- Weather data display -->
+<div class="weather-data-container" transition:fade>
+  <i class={weatherIcon} />
+  <p>
+    {currentTime.toLocaleDateString()}
+    {currentTime.toLocaleTimeString()}
+  </p>
+  <h2>{location}</h2>
+  <h1 class="margin-top-bottom">{temperature}</h1>
+  <h3 class="margin-bottom-0">{weather}</h3>
+  <h4 class="margin-top">{windDescription}</h4>
+  <div class="flex justify-content-center align-items-center">
+    <div class="line" />
+  </div>
+  <div
+    class="additional-weather-data-container flex column justify-content-center align-items-center"
+  >
+    <div class="flex row justify-content-space-between align-items-center">
+      <p>Feels like {feelsLike}</p>
+      <p>|</p>
+      <p>Humidity {humidity}</p>
+    </div>
+    <div class="flex justify-content-center">
+      <p>Wind speed {windSpeed}</p>
+    </div>
+    <div class="flex justify-content-center">
+      <p>Last updated: {lastUpdated}</p>
+    </div>
+  </div>
+</div>
+
+<style>
+  .weather-data-container {
+    text-align: center;
+    padding: 2rem 1.3rem 2.3rem 1.3rem;
+    background: rgba(255, 255, 255, 0.5);
+    box-sizing: border-box;
+    word-spacing: 0.188rem;
+    max-width: 19rem;
+    height: auto;
+  }
+
+  .additional-weather-data-container {
+    border: 0.092rem solid #686868;
+    padding: 0.5rem;
+    box-sizing: border-box;
+  }
+
+  p {
+    margin: 0.3rem;
+  }
+
+  .line {
+    height: 0.15rem;
+    background: #464646;
+    margin-bottom: 3rem;
+    width: 10.3rem;
+  }
+
+  /* Icon styling */
+  [class^='pe-7w-'] {
+    font-size: 4.6rem;
+    color: #464646;
+    padding-bottom: 0.5rem;
+  }
+
+  /* Media queries */
+
+  /* Larger than tablet */
+  @media (min-width: 750px) {
+    .weather-data-container {
+      max-width: 28rem;
+      padding: 3rem 4rem 3.3rem 4rem;
+    }
+
+    .additional-weather-data-container {
+      padding-left: 2rem;
+      padding-right: 2rem;
+    }
+
+    h1 {
+      font-size: 5.125rem;
+    }
+
+    h2 {
+      font-size: 1.625rem;
+    }
+
+    h3 {
+      font-size: 1.375rem;
+    }
+
+    h4 {
+      font-size: 1.125rem;
+    }
+
+    p {
+      font-size: 0.75rem;
+    }
+
+    .margin-top-bottom {
+      margin-top: 1.5rem;
+      margin-bottom: 1.5rem;
+    }
+  }
+</style>
