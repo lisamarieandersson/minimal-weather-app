07ed7fc8d1f635a97891524e6489a1e78bb666e9
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-28 23:16:49 +0200

    moved function fetchWeather to its own file

---
+++ src/weatherAPI.ts
@@ -0,0 +1,14 @@
+/**
+ * Fetches the weather from the API
+ */
+export async function fetchWeather(lat: number, lon: number, apiKey: string) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error(data.message || 'Error fetching weather data.');
+  }
+}
