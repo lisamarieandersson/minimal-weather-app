e27b918c567dc36139aee39ea661308723dc63ad
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-29 15:39:25 +0200

    added comment to function

--- src/weatherAPI.ts
+++ src/weatherAPI.ts
@@ -1,6 +1,4 @@
-/**
- * Fetches the current weather from the API
- */
+// Fetches the current weather from the API
 export async function fetchWeather(lat: number, lon: number, apiKey: string) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
   const response = await fetch(url);
