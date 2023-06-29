559122778f912e322147b623c82714e314f0ef55
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-06-29 05:53:56 +0200

    added interface WeatherData to its own file

---
+++ src/weatherTypes.ts
@@ -0,0 +1,19 @@
+export interface WeatherData {
+  name: string;
+  sys: {
+    country: string;
+  };
+  weather: {
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+  };
+  dt: number;
+}
